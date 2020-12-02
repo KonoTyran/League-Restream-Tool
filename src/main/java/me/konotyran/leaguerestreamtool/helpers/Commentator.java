300959b73204960651f471ebb3b372f6e30f857c
Kono Tyran <Kono.Tyran@gmail.com>
2020-12-01 23:00:58 -0800

    Revert "Revert "Changed to using API.""
    This reverts commit 8558198c83d38b1c0900a01551111151ec810783.

---
+++ src/main/java/me/konotyran/leaguerestreamtool/helpers/Commentator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.konotyran.leaguerestreamtool.helpers;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ *
+ * @author HAklo
+ */
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Commentator {
+    
+    public int id;
+    public boolean approved;
+    public String displayName;
+    public String discordTag;
+    public String publicStream;
+    public String language;
+    
+}
